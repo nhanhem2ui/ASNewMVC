660bc27e7ce72f4683d3b597b29d2dd83587cc49
nhanhem2ui <nhanhem2ui@gmail.com>
2025-11-11 17:11:09 +0000

    gg

--- FUNewsManagement/Pages/NewsArticles/Index.cshtml.cs
+++ FUNewsManagement/Pages/NewsArticles/Index.cshtml.cs
@@ -1,6 +1,11 @@
 using BussinessObject;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FUNewsManagement.Pages.NewsArticles
 {
@@ -13,23 +18,62 @@ namespace FUNewsManagement.Pages.NewsArticles
             _newsArticleService = newsArticleService;
         }
 
-        public IList<NewsArticle> NewsArticles { get; set; }
+        public string TitleSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public void OnGet(int page = 1, int pageSize = 5)
+        public IList<NewsArticle> NewsArticles { get;set; }
+        
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            var articles = _newsArticleService.GetNewsArticles();
-            var totalCount = articles.Count;
-            var pagedArticles = articles
-                .OrderByDescending(a => a.CreatedDate)
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .ToList();
+            CurrentSort = sortOrder;
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
-            ViewData["Page"] = page;
-            ViewData["PageSize"] = pageSize;
-            ViewData["TotalCount"] = totalCount;
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
-            NewsArticles = pagedArticles;
+            CurrentFilter = searchString;
+
+            IQueryable<NewsArticle> newsArticlesIQ = _newsArticleService.GetNewsArticles().AsQueryable();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                newsArticlesIQ = newsArticlesIQ.Where(s => s.NewsTitle.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    newsArticlesIQ = newsArticlesIQ.OrderByDescending(s => s.NewsTitle);
+                    break;
+                case "Date":
+                    newsArticlesIQ = newsArticlesIQ.OrderBy(s => s.CreatedDate);
+                    break;
+                case "date_desc":
+                    newsArticlesIQ = newsArticlesIQ.OrderByDescending(s => s.CreatedDate);
+                    break;
+                default:
+                    newsArticlesIQ = newsArticlesIQ.OrderBy(s => s.NewsTitle);
+                    break;
+            }
+            
+            var pageSize = 5;
+            PageIndex = pageIndex ?? 1;
+            var count = newsArticlesIQ.Count();
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            NewsArticles = newsArticlesIQ.Skip((PageIndex - 1) * pageSize).Take(pageSize).ToList();
         }
     }
 }
